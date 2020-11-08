a4bcf14636de2997f09a6b1528a2dc34e3117520
Hucher <maksim.shishkin.2013@mail.ru>
2020-11-08 17:12:33 +0300

    Registration

---
+++ register.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+require_once 'userRegistration/function.php';
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+//Получить email пользователя,если существует
+$isUserEmail = getEmailUser($pdo ,$email);
+
+if (empty($isUserEmail['email'])) {
+	addUser($email , $password , $pdo);
+	$_SESSION['success'] = 'Регистрация прошла успешна.';
+	setFlashMessage('success' , $_SESSION['success']);
+	if (isset($_SESSION['success'])) {
+		redirectTo('page_login.php');
+		exit();	
+	}		
+}
+else{
+	$_SESSION['danger'] = 'Этот Email адрес занят другим пользователем.';
+	setFlashMessage('danger' , $_SESSION['danger']);
+	redirectTo('page_register.php');
+	exit();
+}
+ 
+
+
+
